b6f03653136c688f342dec559f253fc2456f60ff
Joe45M <joe45moses@gmail.com>
2025-05-21 18:22:27 +0100

    Github integration

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,3 +1,11 @@
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    @if(session()->has('success'))
+        <div class="bg-green-200 p-5 rounded-md border border-green-300 text-green-950">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    <div class="grid lg:grid-cols-2">
+        <livewire:assign-repo></livewire:assign-repo>
+    </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Livewire\Dashboard;
+use http\Env\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::view('/', 'welcome');
 
@@ -12,5 +15,21 @@ Route::get('dashboard', Dashboard::class)
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::get('/github', function () {
+    return Socialite::driver('github')->redirect();
+})->name('socialite.github');
+
+Route::get('/socialite/callback/github', function (\Illuminate\Http\Request $request) {
+    try {
+        $token = Socialite::driver('github')->user()->token;
+        $user = Auth::user();
+        $user->github_token = $token;
+        $user->save();
+        return redirect()->route('dashboard')->with('success', 'GitHub token saved successfully.');
+    } catch (Exception $e) {
+        return redirect()->route('dashboard')->with('error', 'Failed to save GitHub token. Please try again later.');
+    }
+});
 
 require __DIR__.'/auth.php';
