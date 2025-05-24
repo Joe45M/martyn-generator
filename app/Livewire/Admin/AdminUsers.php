52b1396259487964d48fceab21ecae666639c01f
Joe45M <joe45moses@gmail.com>
2025-05-24 06:13:31 +0100

    admin user search

--- app/Livewire/Admin/AdminUsers.php
+++ app/Livewire/Admin/AdminUsers.php
@@ -7,13 +7,32 @@ use Livewire\Component;
 
 class AdminUsers extends Component
 {
+
+    public $name;
+    public $email;
+
     public function render()
     {
 
         $query = User::query();
 
         return view('livewire.admin.admin-users', [
-            'users' => $query->paginate(10),
+            'users' => $this->search(),
         ]);
+    }
+
+    public function search(): \Illuminate\Pagination\LengthAwarePaginator
+    {
+        $query = User::query();
+
+        if ($this->name) {
+            $query->where('name', 'like', '%' . $this->name . '%');
+        }
+
+        if ($this->email) {
+            $query->where('email', 'like', '%' . $this->email . '%');
+        }
+
+        return $query->orderBy('name')->paginate(10);
     }
 }
--- resources/views/livewire/admin/admin-users.blade.php
+++ resources/views/livewire/admin/admin-users.blade.php
@@ -1,4 +1,14 @@
 <x-card title="All registered users">
+
+    <div class="flex gap-3">
+        <div>
+            <x-text-input wire:keyup="search" wire:model="name" placeholder="Name"></x-text-input>
+        </div>
+        <div>
+            <x-text-input wire:keyup="search" wire:model="email" placeholder="Email"></x-text-input>
+        </div>
+    </div>
+
     @foreach($users as $user)
         <div class="flex justify-between items-center p-3 {{ $loop->last ?? 'border-b border-b-gray-300' }}">
             <div class="flex items-center">
@@ -10,4 +20,10 @@
             </div>
         </div>
     @endforeach
+
+    @if($users->isEmpty())
+        <div class="p-5">
+            <p class="text-gray-500">No users found.</p>
+        </div>
+    @endif
 </x-card>
